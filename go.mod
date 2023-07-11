8bf85891ea2f9deef6a480bef4a2ce6da5aa1b96
xnzone <xnzone@qq.com>
2023-07-11 05:05:36 -0400

    update douyin miniapp openapi

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/dev-openapi/douyin-miniapp
+
+go 1.18
+
+require (
+	github.com/golang/protobuf v1.5.3
+	google.golang.org/genproto/googleapis/api v0.0.0-20230710151506-e685fd7b542b
+)
+
+require (
+	google.golang.org/genproto v0.0.0-20230706204954-ccb25ca9f130 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ option.go
@@ -0,0 +1,94 @@
+package douyin_miniapp
+
+import (
+	"net/http"
+	"errors"
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Option func(*Options)
+
+type FnRequest func(*http.Client,*http.Request) (*http.Response, error)
+type FnResponse func(*http.Response, interface{}) error
+
+var (
+	ErrNil = errors.New("resp nil")
+	ErrNot200 = errors.New("resp not 200")
+)
+
+type Options struct {
+	// do request
+	DoRequest FnRequest
+	// do response
+	DoResponse FnResponse
+	// addr
+	addr string
+	// client
+	client *http.Client
+}
+
+func newOptions(opts ...Option) *Options {
+	opt := Options{
+		client: http.DefaultClient,
+		DoRequest: doRequest,
+		DoResponse: doResponse,
+	}
+	for _, o := range opts {
+		o(&opt)
+	}
+	return &opt
+}
+
+func buildOptions(opt *Options, opts ...Option) *Options {
+	res := newOptions(opts...)
+	if len(res.addr) == 0 {
+		res.addr = opt.addr
+	}
+	return res
+}
+
+func doRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	return client.Do(req)
+}
+
+func doResponse(resp *http.Response, a interface{}) error {
+	if resp == nil {
+		return ErrNil
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return ErrNot200
+	}
+	defer func(){ _ = resp.Body.Close()}()
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bs, a)
+}
+
+func WithDoRequest(fn FnRequest) Option {
+	return func(o *Options) {
+		o.DoRequest = fn
+	}
+}
+
+func WithDoResponse(fn FnResponse) Option {
+	return func(o *Options) {
+		o.DoResponse = fn
+	}
+}
+
+func WithClient(c *http.Client) Option {
+	return func(o *Options) {
+		o.client = c
+	}
+}
+
+// addr must start with https:// or http://
+func WithAddr(addr string) Option {
+	return func(o *Options) {
+		o.addr = addr
+	}
+}
+
